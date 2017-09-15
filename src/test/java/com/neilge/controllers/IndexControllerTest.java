206b8a3dfcddbb3bcebcff84fcfcd0769a2cb217
neilge <neilge@audible.com>
2017-09-14 23:09:41 -0400

    Add test code to project

---
+++ src/test/java/com/neilge/controllers/IndexControllerTest.java
@@ -0,0 +1,15 @@
+package com.neilge.controllers;
+
+
+import org.junit.Test;
+
+/**
+ * Created by neilge on 9/14/17.
+ */
+public class IndexControllerTest {
+
+    @Test
+    public void dummyTest() {
+
+    }
+}
